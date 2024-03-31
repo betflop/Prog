59187527c6360c7476d3ee205a9a6ec4b0ca9cb2
betflop <pawwlyk52@gmail.com>
2024-03-31 19:36:03 +0300

    exec command

--- rtty/cmd/run.go
+++ rtty/cmd/run.go
@@ -1,29 +1,29 @@
 package cmd
 
 import (
-	"context"
-	"embed"
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/fs"
-	"log"
-	"net/http"
-	"os"
-	"os/exec"
-	"os/signal"
-	"strconv"
-	"strings"
-	"syscall"
-	"text/template"
-	"time"
-	"unicode/utf8"
+    "context"
+    "embed"
+    "encoding/json"
+    "fmt"
+    "io"
+    "io/fs"
+    "log"
+    "net/http"
+    "os"
+    "os/exec"
+    "os/signal"
+    "strconv"
+    "strings"
+    "syscall"
+    "text/template"
+    "time"
+    "unicode/utf8"
 
-	_ "embed"
+    _ "embed"
 
-	"github.com/creack/pty"
-	"github.com/spf13/cobra"
-	"golang.org/x/net/websocket"
+    "github.com/creack/pty"
+    "github.com/spf13/cobra"
+    "golang.org/x/net/websocket"
 )
 
 //go:embed public/index.html
@@ -45,14 +45,14 @@ var checkProcInterval = 5
 type Event string
 
 const (
-	EventResize  Event = "resize"
-	EventSnedkey Event = "sendKey"
-	EventClose   Event = "close"
+    EventResize  Event = "resize"
+    EventSnedkey Event = "sendKey"
+    EventClose   Event = "close"
 )
 
 type Message struct {
-	Event Event
-	Data  interface{}
+    Event Event
+    Data  interface{}
 }
 
 var ptmx *os.File
@@ -83,278 +83,294 @@ func cleanEnv() {
 }
 
 func run(ws *websocket.Conn) {
-	defer ws.Close()
+    defer ws.Close()
 
-	wsconn := &wsConn{
-		conn: ws,
-	}
+    wsconn := &wsConn{
+        conn: ws,
+    }
 
-	if ptmx == nil {
-		var msg Message
-		if err := json.NewDecoder(ws).Decode(&msg); err != nil {
-			log.Println("failed to decode message:", err)
-			return
-		}
+    if ptmx == nil {
+        var msg Message
+        if err := json.NewDecoder(ws).Decode(&msg); err != nil {
+            log.Println("failed to decode message:", err)
+            return
+        }
+        rows, cols, err := windowSize(msg.Data)
+        if err != nil {
+            _, _ = ws.Write([]byte(fmt.Sprintf("%s\r\n", err)))
+            return
+        }
+        winsize := &pty.Winsize{
+            Rows: rows,
+            Cols: cols,
+        }
 
-		rows, cols, err := windowSize(msg.Data)
-		if err != nil {
-			_, _ = ws.Write([]byte(fmt.Sprintf("%s\r\n", err)))
-			return
-		}
-		winsize := &pty.Winsize{
-			Rows: rows,
-			Cols: cols,
-		}
-        
-		c := filter(strings.Split(command, " "))
-		if len(c) > 1 {
-			//nolint
+        c := filter(strings.Split(command, " "))
+        if len(c) > 1 {
+            //nolint
             println("exec cmd", c[0], c[1:])
-			execCmd = exec.Command(c[0], c[1:]...)
-		} else {
-			//nolint
+            execCmd = exec.Command(c[0], c[1:]...)
+        } else {
+            //nolint
             cleanEnv()
-			execCmd = exec.Command(c[0])
+            execCmd = exec.Command(c[0])
             println("clean env ", c[0])
-			// execCmd = exec.Command("env", "-i", "zsh")
-		}
+            // execCmd = exec.Command("env", "-i", "zsh")
+        }
 
-		ptmx, err = pty.StartWithSize(execCmd, winsize)
-		if err != nil {
-			log.Println("failed to create pty", err)
-			return
-		}
-	}
+        ptmx, err = pty.StartWithSize(execCmd, winsize)
+        if err != nil {
+            log.Println("failed to create pty", err)
+            return
+        }
+    }
 
-	// write data to process
-	go func() {
-		for {
-			var msg Message
-			if err := json.NewDecoder(ws).Decode(&msg); err != nil {
-				return
-			}
+    // write data to process
+    go func() {
+        var inputBuffer strings.Builder
+        for {
+            var msg Message
+            if err := json.NewDecoder(ws).Decode(&msg); err != nil {
+                return
+            }
 
-			if msg.Event == EventClose {
-				log.Println("close websocket")
-				ws.Close()
-				return
-			}
+            if msg.Event == EventClose {
+                log.Println("close websocket")
+                ws.Close()
+                return
+            }
 
-			if msg.Event == EventResize {
-				rows, cols, err := windowSize(msg.Data)
-				if err != nil {
-					log.Println(err)
-					return
-				}
+            if msg.Event == EventResize {
+                rows, cols, err := windowSize(msg.Data)
+                if err != nil {
+                    log.Println(err)
+                    return
+                }
 
-				winsize := &pty.Winsize{
-					Rows: rows,
-					Cols: cols,
-				}
+                winsize := &pty.Winsize{
+                    Rows: rows,
+                    Cols: cols,
+                }
 
-				if err := pty.Setsize(ptmx, winsize); err != nil {
-					log.Println("failed to set window size:", err)
-					return
-				}
-				continue
-			}
+                if err := pty.Setsize(ptmx, winsize); err != nil {
+                    log.Println("failed to set window size:", err)
+                    return
+                }
+                continue
+            }
 
-			data, ok := msg.Data.(string)
-			if !ok {
-				log.Println("invalid message data:", data)
-				return
-			}
+            data, ok := msg.Data.(string)
+            if !ok {
+                log.Println("invalid message data:", data)
+                return
+            }
+            // --------------------------------------------
+            // Внутри цикла, где вы читаете данные от пользователя
+            cdata, ok := msg.Data.(string)
+            if !ok {
+                log.Println("invalid message data:", cdata)
+                return
+            }
+            // Добавляем ввод пользователя в буфер
+            inputBuffer.WriteString(cdata)
+            // Проверяем, содержит ли буфер символ новой строки
+            if strings.Contains(inputBuffer.String(), "\r") {
+                // Выводим команду
+                fmt.Println("Executing command:", inputBuffer.String())
+                // Очищаем буфер
+                inputBuffer.Reset()
+            }
+            // --------------------------------------------
+            _, err := ptmx.WriteString(data)
+            if err != nil {
+                log.Println("failed to write data to ptmx:", err)
+                return
+            }
+        }
+    }()
 
-			_, err := ptmx.WriteString(data)
-			if err != nil {
-				log.Println("failed to write data to ptmx:", err)
-				return
-			}
-		}
-	}()
-
-	_, _ = io.Copy(wsconn, ptmx)
+    _, _ = io.Copy(wsconn, ptmx)
 }
 
 type wsConn struct {
-	conn *websocket.Conn
-	buf  []byte
+    conn *websocket.Conn
+    buf  []byte
 }
 
 // Checking and buffering `b`
 // If `b` is invalid UTF-8, it would be buffered
 // if buffer is valid UTF-8, it would write to connection
 func (ws *wsConn) Write(b []byte) (i int, err error) {
-	if !utf8.Valid(b) {
-		buflen := len(ws.buf)
-		blen := len(b)
-		ws.buf = append(ws.buf, b...)[:buflen+blen]
-		if utf8.Valid(ws.buf) {
-			log.Println("utf write log --- ", ws.buf)
-			_, e := ws.conn.Write(ws.buf)
-			ws.buf = ws.buf[:0]
-			return blen, e
-		}
-		return blen, nil
-	}
+    if !utf8.Valid(b) {
+        buflen := len(ws.buf)
+        blen := len(b)
+        ws.buf = append(ws.buf, b...)[:buflen+blen]
+        if utf8.Valid(ws.buf) {
+            log.Println("utf write log --- ", ws.buf)
+            _, e := ws.conn.Write(ws.buf)
+            ws.buf = ws.buf[:0]
+            return blen, e
+        }
+        return blen, nil
+    }
 
-	if len(ws.buf) > 0 {
+    if len(ws.buf) > 0 {
 
-		log.Println("ws buf > 0 --- ", ws.buf)
-		n, err := ws.conn.Write(ws.buf)
-		ws.buf = ws.buf[:0]
-		if err != nil {
-			return n, err
-		}
-	}
-	log.Println("ws.conn.Write --- ", ws.buf)
-	n, e := ws.conn.Write(b)
-	return n, e
+        log.Println("ws buf > 0 --- ", ws.buf)
+        n, err := ws.conn.Write(ws.buf)
+        ws.buf = ws.buf[:0]
+        if err != nil {
+            return n, err
+        }
+    }
+    log.Println("ws.conn.Write --- ", ws.buf)
+    n, e := ws.conn.Write(b)
+    return n, e
 }
 
 var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "Run command",
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) > 0 {
-			command = args[0]
-		}
-		portFlag, err := cmd.PersistentFlags().GetInt("port")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		port := strconv.Itoa(portFlag)
+    Use:   "run",
+    Short: "Run command",
+    Run: func(cmd *cobra.Command, args []string) {
+        if len(args) > 0 {
+            command = args[0]
+        }
+        portFlag, err := cmd.PersistentFlags().GetInt("port")
+        if err != nil {
+            log.Println(err)
+            return
+        }
+        port := strconv.Itoa(portFlag)
 
-		font, err := cmd.PersistentFlags().GetString("font")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		fontSize, err := cmd.PersistentFlags().GetString("font-size")
-		if err != nil {
-			log.Println(err)
-			return
-		}
+        font, err := cmd.PersistentFlags().GetString("font")
+        if err != nil {
+            log.Println(err)
+            return
+        }
+        fontSize, err := cmd.PersistentFlags().GetString("font-size")
+        if err != nil {
+            log.Println(err)
+            return
+        }
 
-		addr, err := cmd.PersistentFlags().GetString("addr")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		if addr == "" {
-			// addr = "localhost"
-			addr = "0.0.0.0"
-		}
+        addr, err := cmd.PersistentFlags().GetString("addr")
+        if err != nil {
+            log.Println(err)
+            return
+        }
+        if addr == "" {
+            // addr = "localhost"
+            addr = "0.0.0.0"
+        }
 
-		indexJS = strings.Replace(indexJS, "{addr}", template.JSEscapeString(addr), 1)
-		indexJS = strings.Replace(indexJS, "{port}", port, 1)
-		indexJS = strings.Replace(indexJS, "{fontFamily}", template.JSEscapeString(font), 1)
-		indexJS = strings.Replace(indexJS, "{fontSize}", template.JSEscapeString(fontSize), 1)
+        indexJS = strings.Replace(indexJS, "{addr}", template.JSEscapeString(addr), 1)
+        indexJS = strings.Replace(indexJS, "{port}", port, 1)
+        indexJS = strings.Replace(indexJS, "{fontFamily}", template.JSEscapeString(font), 1)
+        indexJS = strings.Replace(indexJS, "{fontSize}", template.JSEscapeString(fontSize), 1)
 
-		var serverErr error
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			_, _ = w.Write([]byte(indexHTML))
-		})
-		sub, err := fs.Sub(public, "public")
-		if err != nil {
-			return
-		}
-		publicHandler := http.FileServer(http.FS(sub))
-		mux.Handle("/css/", publicHandler)
-		mux.Handle("/js/", publicHandler)
-		mux.HandleFunc("/index.js", func(w http.ResponseWriter, r *http.Request) {
-			_, _ = w.Write([]byte(indexJS))
-		})
-		mux.Handle("/ws", websocket.Handler(run))
+        var serverErr error
+        mux := http.NewServeMux()
+        mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+            _, _ = w.Write([]byte(indexHTML))
+        })
+        sub, err := fs.Sub(public, "public")
+        if err != nil {
+            return
+        }
+        publicHandler := http.FileServer(http.FS(sub))
+        mux.Handle("/css/", publicHandler)
+        mux.Handle("/js/", publicHandler)
+        mux.HandleFunc("/index.js", func(w http.ResponseWriter, r *http.Request) {
+            _, _ = w.Write([]byte(indexJS))
+        })
+        mux.Handle("/ws", websocket.Handler(run))
 
-		server := &http.Server{
-			Addr:    addr + ":" + port,
-			Handler: mux,
-		}
+        server := &http.Server{
+            Addr:    addr + ":" + port,
+            Handler: mux,
+        }
 
-		go func() {
-			log.Println("running command: " + command)
-			log.Printf("running http://%s:%s\n", addr, port)
+        go func() {
+            log.Println("running command: " + command)
+            log.Printf("running http://%s:%s\n", addr, port)
 
-			if serverErr := server.ListenAndServe(); serverErr != nil {
-				log.Println(serverErr)
-			}
-		}()
+            if serverErr := server.ListenAndServe(); serverErr != nil {
+                log.Println(serverErr)
+            }
+        }()
 
-		// check process state
-		go func() {
-			ticker := time.NewTicker(time.Duration(checkProcInterval) * time.Second)
-			for range ticker.C {
-				if execCmd != nil {
-					state, err := execCmd.Process.Wait()
-					if err != nil {
-						return
-					}
+        // check process state
+        go func() {
+            ticker := time.NewTicker(time.Duration(checkProcInterval) * time.Second)
+            for range ticker.C {
+                if execCmd != nil {
+                    state, err := execCmd.Process.Wait()
+                    if err != nil {
+                        return
+                    }
 
-					if state.ExitCode() != -1 {
-						ptmx.Close()
-						ptmx = nil
-						execCmd = nil
-					}
-				}
-			}
-		}()
+                    if state.ExitCode() != -1 {
+                        ptmx.Close()
+                        ptmx = nil
+                        execCmd = nil
+                    }
+                }
+            }
+        }()
 
-		// wait for run server
-		time.Sleep(time.Duration(waitTime) * time.Microsecond)
+        // wait for run server
+        time.Sleep(time.Duration(waitTime) * time.Microsecond)
 
-		if serverErr == nil {
-			// open browser
-			openView, err := cmd.PersistentFlags().GetBool("view")
-			if err != nil {
-				log.Println(err)
-			} else if openView {
-				OpenBrowser(fmt.Sprintf("http://%s:%s", addr, port))
-			}
-		}
+        if serverErr == nil {
+            // open browser
+            openView, err := cmd.PersistentFlags().GetBool("view")
+            if err != nil {
+                log.Println(err)
+            } else if openView {
+                OpenBrowser(fmt.Sprintf("http://%s:%s", addr, port))
+            }
+        }
 
-		quit := make(chan os.Signal, 1)
-		signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
-		<-quit
+        quit := make(chan os.Signal, 1)
+        signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+        <-quit
 
-		if ptmx != nil {
-			_ = ptmx.Close()
-		}
-		if execCmd != nil {
-			_ = execCmd.Process.Kill()
-			_, _ = execCmd.Process.Wait()
-		}
-		if err := server.Shutdown(context.Background()); err != nil {
-			log.Println("failed to shutdown server", err)
-		}
-	},
+        if ptmx != nil {
+            _ = ptmx.Close()
+        }
+        if execCmd != nil {
+            _ = execCmd.Process.Kill()
+            _, _ = execCmd.Process.Wait()
+        }
+        if err := server.Shutdown(context.Background()); err != nil {
+            log.Println("failed to shutdown server", err)
+        }
+    },
 }
 
 func init() {
-	runCmd.PersistentFlags().IntP("port", "p", 9999, "server port")
-	runCmd.PersistentFlags().StringP("addr", "a", "", "server address")
-	runCmd.PersistentFlags().String("font", "", "font")
-	runCmd.PersistentFlags().String("font-size", "", "font size")
-	runCmd.PersistentFlags().BoolP("view", "v", false, "open browser")
-	runCmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
-		fmt.Printf(`Run command
+    runCmd.PersistentFlags().IntP("port", "p", 9999, "server port")
+    runCmd.PersistentFlags().StringP("addr", "a", "", "server address")
+    runCmd.PersistentFlags().String("font", "", "font")
+    runCmd.PersistentFlags().String("font-size", "", "font size")
+    runCmd.PersistentFlags().BoolP("view", "v", false, "open browser")
+    runCmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+        fmt.Printf(`Run command
 
-Usage:
-  rtty run [command] [flags]
+        Usage:
+        rtty run [command] [flags]
 
-Command
-  Execute specified command (default "%s")
+        Command
+        Execute specified command (default "%s")
 
-Flags:
-  -a, --addr string        server address
-      --font string        font
-      --font-size string   font size
-  -h, --help               help for run
-  -p, --port int           server port (default 9999)
-  -v, --view               open browser
-`, command)
-	})
-	rootCmd.AddCommand(runCmd)
+        Flags:
+        -a, --addr string        server address
+        --font string        font
+        --font-size string   font size
+        -h, --help               help for run
+        -p, --port int           server port (default 9999)
+        -v, --view               open browser
+        `, command)
+    })
+    rootCmd.AddCommand(runCmd)
 }
